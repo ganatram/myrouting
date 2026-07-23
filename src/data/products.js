d1e402a65ff608185fadd869122e7dc253a25041
Mukesh <ganatram@gmail.com>
2026-07-23 12:06:37 +0530

    myrouting

--- src/data/products.js
+++ src/data/products.js
@@ -1,20 +1,33 @@
 export const products = [
   {
-    description: "A collection of navigational components",
+    description:
+      "A collection of navigational components that compose declaratively with your app",
     id: 1,
     name: "React Router",
     price: 8,
   },
   {
-    description: "Library that manages state",
+    description: "A library that helps manage state across your app",
     id: 2,
     name: "React Redux",
     price: 12,
   },
   {
-    description: "Library to manage forms",
+    description: "A library that helps you implement robust forms",
     id: 3,
     name: "React Hook Form",
     price: 9,
+  },
+  {
+    description: "A library that helps you interact with a REST API",
+    id: 4,
+    name: "React Apollo",
+    price: 10,
+  },
+  {
+    description: "A library that provides utility CSS classes",
+    id: 5,
+    name: "Tailwind CSS",
+    price: 7,
   },
 ];
