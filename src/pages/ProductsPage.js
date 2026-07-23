d3cacdcceba8d4d984ac5ec6fb7451f5b4f22fe7
Mukesh <ganatram@gmail.com>
2026-07-23 16:21:30 +0530

    myrouting

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,4 +1,5 @@
 import { products } from "../data/products";
+import { Link } from "react-router-dom";
 
 export function ProductsPage() {
   return (
@@ -6,7 +7,9 @@ export function ProductsPage() {
       <h2>Here are some great tools work with react </h2>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.name}</li>
+          <li key={product.id}>
+            <Link to={`${product.id}`}>{product.name}</Link>
+          </li>
         ))}
       </ul>
     </div>
