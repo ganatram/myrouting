6b3640b2291d51bba55e2c572422dd8e00958141
Mukesh <ganatram@gmail.com>
2026-07-23 15:49:43 +0530

    myrouting

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { ProductsPage } from "./pages/ProductsPage";
 import { App } from "./App";
+import { ProductPage } from "./pages/ProductPage";
 
 const myRouter = createBrowserRouter([
   // RouterTree (root level)
@@ -14,7 +15,10 @@ const myRouter = createBrowserRouter([
         path: "products",
         element: <ProductsPage />,
       },
-      {},
+      {
+        path: "products/:id", // shared Object {id:5}
+        element: <ProductPage />,
+      },
     ],
   },
 ]);
---
+++ src/pages/ProductPage.js
@@ -0,0 +1,29 @@
+import { useParams } from "react-router-dom";
+import { products } from "../data/products";
+
+export function ProductPage() {
+  const Params = useParams(); // {id:5}
+  const id = Params.id === undefined ? undefined : parseInt(Params.id);
+  //console.log(id);
+  const product = products.find((product) => product.id === id);
+  // console.log(product);
+
+  return (
+    <div>
+      {product === undefined ? (
+        <h1>Unknown product</h1>
+      ) : (
+        <>
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+          <p>
+            {new Intl.NumberFormat("en-US", {
+              currency: "USD",
+              style: "currency",
+            }).format(product.price)}
+          </p>
+        </>
+      )}
+    </div>
+  );
+}
